0e8cea344ba59e893846ea86ed2ffe4773faf8fb
DeepakMahto99999 <mahtodeepak239@gmail.com>
2025-07-07 10:21:13 +0530

    routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,31 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import MyOrders from './pages/MyOrders'
+import About from './pages/About'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Login from './pages/Login'
+import PageNotFound from './pages/pageNotFound'  
+import Cart from './pages/Cart'
+
 
 const App = () => {
   return (
-    <div className="text-2xl text-black-100">
-      App
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/Collections' element={<Collection />} />
+        <Route path='/Contact' element={<Contact />} />
+        <Route path='/MyOrders' element={<MyOrders />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='*' element={<PageNotFound />} /> 
+      </Routes>
+      <Footer/>
     </div>
   )
 }
---
+++ client/src/pages/PageNotFound.jsx
@@ -0,0 +1,10 @@
+
+ const PageNotFound = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold flex items-center justify-center h-screen ">404 - Page Not Found</h1>
+    </div>
+  )
+}
+
+export default PageNotFound
